924cac162ecfc7084a61e4d50094658cf3615397
Nick Ott (njo12) <nickott13@yahoo.com>
2021-11-03 18:59:24 -0400

    Gettting user ids

--- src/App.java
+++ src/App.java
@@ -1,16 +1,21 @@
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.json.JSONObject;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
 
 public class App 
 {
     public static void main( String[] args )
     {
+    	//Test stuff
     	MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
 
 		MongoDatabase database = mongoClient.getDatabase( "data" );
@@ -21,6 +26,22 @@ public class App
 		
 		JSONObject firstDoc = new JSONObject(collection.find().first().toJson());
 		System.out.println(firstDoc.get("name").toString());
+		
+		//Find all user id
+		List<Document> userIdList = collection.find().projection(Projections.fields(Projections.include("user_id"))).into(new ArrayList<Document>());
+		for (int i = 0; i < 20; i++) {
+			Document userId = userIdList.get(i);
+            System.out.println(userId.get("user_id"));
+        }
+		
+		ArrayList<User> userList = new ArrayList<User>();
+		//for() {
+			//User newUser = new User(userId);
+		//}
+		
+		//for(User user: userList) {
+			
+		//}
 		
 		//To close connection
 		mongoClient.close();
